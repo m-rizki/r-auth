963e577e4cea81c11b65f5992bdc1f48bafc6610
m-rizki <37399436+m-rizki@users.noreply.github.com>
2025-05-21 12:01:23 +0700

    Add login, refresh-token, and logout handler

--- front-end/app/routes/home.tsx
+++ front-end/app/routes/home.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/home";
 import { AlertCircle, KeyRound } from "lucide-react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { serverUrl } from "utils/server-util";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +12,38 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+type FormDataType = {
+  username: string;
+  password: string;
+};
+
 export default function Home() {
+  const {
+    reset,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormDataType>({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const submitHandler = async (data: FormDataType) => {
+    const payload = {
+      username: data.username,
+      password: data.password,
+    };
+
+    const url = serverUrl + "/login";
+    const response = await axios.post(url, payload, { withCredentials: true });
+    console.log(response.data);
+
+    reset({ username: "", password: "" });
+    console.log(payload);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse lg:gap-16">
@@ -41,32 +75,59 @@ export default function Home() {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Username</label>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Username"
-              />
-              <label className="label">Password</label>
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="Password"
-              />
-              <button
-                type="button"
-                className="btn btn-accent btn-outline btn-sm mt-4"
-              >
-                Login
-              </button>
-              {/* <button
-                type="button"
-                className="btn btn-error btn-outline btn-sm mt-4"
-              >
-                Logout
-              </button> */}
-            </fieldset>
+            <form onSubmit={handleSubmit(submitHandler)}>
+              <fieldset className="fieldset gap-4">
+                <div className="flex flex-col gap-1">
+                  <label
+                    className={`label ${errors.username ? "text-error" : ""}`}
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    className={`input w-full ${errors.username ? "border-error" : ""}`}
+                    placeholder="Username"
+                    {...register("username", {
+                      required: "username is required",
+                    })}
+                  />
+                  {errors.username && (
+                    <p className="text-error text-xs">
+                      {errors.username.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <label
+                    className={`label ${errors.password ? "text-error" : ""}`}
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className={`input w-full ${errors.password ? "border-error" : ""}`}
+                    placeholder="Password"
+                    {...register("password", {
+                      required: "password is required",
+                    })}
+                  />
+                  {errors.password && (
+                    <p className="text-error text-xs">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn btn-accent btn-outline btn-sm mt-4"
+                >
+                  Login
+                </button>
+              </fieldset>
+            </form>
+
             <p className="text-base-content/60 mt-4 text-center text-xs">
               <a className="link link-hover" href="/web_client_licenses.txt">
                 GNU General Public License v3.0
--- front-end/app/routes/protected-page.tsx
+++ front-end/app/routes/protected-page.tsx
@@ -1,14 +1,49 @@
+import axios from "axios";
+import { serverUrl } from "utils/server-util";
+
 export default function ProtectedPage() {
+  const handleMe = () => {
+    const response = axios.get(serverUrl + "/me", { withCredentials: true });
+    console.log(response);
+  };
+
+  const handleLogout = () => {
+    const response = axios.post(serverUrl + "/logout", null, {
+      withCredentials: true,
+    });
+    console.log(response);
+  };
+
+  const handleRefreshToken = () => {
+    const response = axios.post(serverUrl + "/refresh-token", null, {
+      withCredentials: true,
+    });
+    console.log(response);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col">
-        <p>Protected Page</p>
+        <p className="text-2xl font-semibold">Protected Page</p>
         <div className="mt-8 flex flex-row gap-10">
-          <button className="btn btn-outline btn-primary btn-sm">me</button>
-          <button className="btn btn-outline btn-accent btn-sm">
+          <button
+            className="btn btn-outline btn-primary btn-sm"
+            onClick={handleMe}
+          >
+            me
+          </button>
+          <button
+            className="btn btn-outline btn-accent btn-sm"
+            onClick={handleRefreshToken}
+          >
             refresh-token
           </button>
-          <button className="btn btn-outline btn-error btn-sm">logout</button>
+          <button
+            className="btn btn-outline btn-error btn-sm"
+            onClick={handleLogout}
+          >
+            logout
+          </button>
         </div>
       </div>
     </div>
